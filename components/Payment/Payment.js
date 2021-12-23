340047c73b56daffca1f8e689a3e8a5f5422d85c
Pranta07 <palpranta@gmail.com>
2021-12-24 03:41:19 +0600

    updating payment status done

--- components/Payment/Payment.js
+++ components/Payment/Payment.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { Button } from "react-native-paper";
-import { useNavigate } from "react-router";
+import { ActivityIndicator, Button, Colors } from "react-native-paper";
+import { useNavigate, useParams } from "react-router";
 import useAuth from "../../hooks/useAuth";
 
 const Payment = () => {
     const [myOrders, setMyOrders] = useState([]);
+    const [loading, setLoading] = useState(true);
     const { user } = useAuth();
 
     const navigate = useNavigate();
 
     useEffect(() => {
+        setLoading(true);
         fetch(
             `https://mighty-thicket-60343.herokuapp.com/orders/${user?.email}`
         )
@@ -18,19 +20,39 @@ const Payment = () => {
             .then((data) => {
                 // console.log(data);
                 setMyOrders(data);
+                setLoading(false);
             });
     }, []);
 
     const handlePay = () => {
-        //payment status update korte hobe db te.
-        alert("Payment Success!");
-        // navigate("/trackMyOrders");
+        //updating payment status
+        fetch(`https://mighty-thicket-60343.herokuapp.com/pay/${user.email}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                if (result.modifiedCount) {
+                    alert("Payment Success!");
+                    navigate("/trackMyOrders");
+                }
+            });
     };
 
     const totalPrice = myOrders.reduce(
         (previous, current) => previous + current.price,
         0
     );
+
+    if (loading) {
+        return (
+            <View style={styles.loading}>
+                <ActivityIndicator animating={true} color={Colors.blue800} />
+            </View>
+        );
+    }
 
     return (
         <View style={styles.container}>
@@ -55,6 +77,11 @@ const Payment = () => {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    loading: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
