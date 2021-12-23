bcf81cc97bb0315b51197d1cfb2affa64b3296c0
Pranta07 <palpranta@gmail.com>
2021-12-24 01:34:27 +0600

    tracking my orders done

--- components/TrackMyOrders/TrackMyOrders.js
+++ components/TrackMyOrders/TrackMyOrders.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+    ActivityIndicator,
+    Avatar,
+    Card,
+    Colors,
+    IconButton,
+} from "react-native-paper";
 import useAuth from "../../hooks/useAuth";
 
 const TrackMyOrders = () => {
@@ -7,25 +14,91 @@ const TrackMyOrders = () => {
     const [loading, setLoading] = useState(true);
     const { user } = useAuth();
 
+    const mergeOrders = [];
+    myOrders?.forEach((order) => {
+        const items = order.orderedItems;
+        items?.forEach((item) => {
+            mergeOrders.push(item);
+        });
+    });
+
     useEffect(() => {
         setLoading(true);
-        fetch(
-            `https://mighty-thicket-60343.herokuapp.com/orders/${user?.email}`
-        )
+        fetch(`https://mighty-thicket-60343.herokuapp.com/orders/${user.email}`)
             .then((res) => res.json())
             .then((data) => {
                 // console.log(data);
                 setMyOrders(data);
                 setLoading(false);
             });
-    }, []);
+    }, [user.email]);
+
+    const renderItem = ({ item }) => {
+        return (
+            <View style={styles.item}>
+                <Card style={styles.card}>
+                    <Card.Title
+                        title={item?.name}
+                        subtitle={`Status: Pending`}
+                        left={(props) => (
+                            <Avatar.Image
+                                {...props}
+                                size={40}
+                                source={{
+                                    uri: item?.img,
+                                }}
+                            />
+                        )}
+                        right={(props) => (
+                            <View style={{ flex: 1 }}>
+                                <IconButton {...props} icon="food" />
+                                <Text>{item.quantity}</Text>
+                            </View>
+                        )}
+                    />
+                </Card>
+            </View>
+        );
+    };
+
+    if (loading) {
+        return (
+            <View style={styles.loading}>
+                <ActivityIndicator animating={true} color={Colors.blue800} />
+            </View>
+        );
+    }
+
     return (
-        <View>
-            <Text>Track my orders!</Text>
+        <View style={styles.container}>
+            <Text>You have Ordered {mergeOrders.length} Items!</Text>
+            <FlatList
+                data={mergeOrders}
+                keyExtractor={(item) => item.name}
+                renderItem={renderItem}
+                numColumns={1}
+            />
         </View>
     );
 };
 
 export default TrackMyOrders;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+    },
+    loading: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    card: {
+        margin: 5,
+        borderRadius: 5,
+    },
+    item: {
+        width: 350,
+    },
+});
