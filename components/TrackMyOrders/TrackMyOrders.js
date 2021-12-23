e47119a8b87b4c139302797c73a85491cd0f0504
Pranta07 <palpranta@gmail.com>
2021-12-24 00:39:51 +0600

    loading state added

--- components/TrackMyOrders/TrackMyOrders.js
+++ components/TrackMyOrders/TrackMyOrders.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import useAuth from "../../hooks/useAuth";
 
 const TrackMyOrders = () => {
+    const [myOrders, setMyOrders] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { user } = useAuth();
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(
+            `https://mighty-thicket-60343.herokuapp.com/orders/${user?.email}`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                // console.log(data);
+                setMyOrders(data);
+                setLoading(false);
+            });
+    }, []);
     return (
         <View>
             <Text>Track my orders!</Text>
