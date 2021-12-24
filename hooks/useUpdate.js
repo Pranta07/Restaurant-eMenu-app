4b86d20fc325ea0fd44a8b56b03b32ab54952305
Pranta07 <palpranta@gmail.com>
2021-12-24 20:31:11 +0600

    data type updated

--- hooks/useUpdate.js
+++ hooks/useUpdate.js
@@ -1,10 +1,11 @@
 const useUpdate = (status, email) => {
+    const data = { status };
     fetch(`https://mighty-thicket-60343.herokuapp.com/food/${email}`, {
         method: "PUT",
         headers: {
             "content-type": "application/json",
         },
-        body: JSON.stringify(status),
+        body: JSON.stringify(data),
     })
         .then((res) => res.json())
         .then((result) => {
