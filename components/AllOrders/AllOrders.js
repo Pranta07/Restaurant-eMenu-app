9ebe7e1c9c782ad294d21096db47728b0f14b430
Pranta07 <palpranta@gmail.com>
2021-12-25 02:01:02 +0600

    all orders page done

--- components/AllOrders/AllOrders.js
+++ components/AllOrders/AllOrders.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+    ActivityIndicator,
+    Avatar,
+    Badge,
+    Card,
+    IconButton,
+} from "react-native-paper";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const AllOrders = () => {
     const [allOrders, setAllOrders] = useState([]);
@@ -16,13 +24,130 @@ const AllOrders = () => {
             });
     }, []);
 
+    const renderItem = ({ item }) => {
+        const items = item.orderedItems;
+        return (
+            <View style={styles.box}>
+                <View
+                    style={{
+                        flex: 1,
+                        alignItems: "center",
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontSize: 16,
+                            marginVertical: 2,
+                        }}
+                    >
+                        Order Id: {item._id}
+                    </Text>
+                    <Text
+                        style={{
+                            fontSize: 16,
+                            marginVertical: 2,
+                        }}
+                    >
+                        Customer Email: {item.email}
+                    </Text>
+                    <Text
+                        style={{
+                            fontSize: 16,
+                            marginVertical: 2,
+                        }}
+                    >
+                        Payment Status:{" "}
+                        {item.paymentStatus ? "Paid" : "Pending"}
+                    </Text>
+                </View>
+                {items.map((elem) => (
+                    <Card key={elem?.name} style={styles.card}>
+                        <Card.Title
+                            title={elem?.name}
+                            subtitle={`Food Status: ${item?.foodStatus}`}
+                            left={(props) => (
+                                <Avatar.Image
+                                    {...props}
+                                    size={40}
+                                    source={{
+                                        uri: elem?.img,
+                                    }}
+                                />
+                            )}
+                            right={(props) => (
+                                <View
+                                    style={{
+                                        flex: 1,
+                                        flexDirection: "row",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    <IconButton {...props} icon="food" />
+                                    <Badge
+                                        style={{
+                                            marginBottom: 25,
+                                            marginRight: 10,
+                                        }}
+                                    >
+                                        X {elem.quantity}
+                                    </Badge>
+                                </View>
+                            )}
+                        />
+                    </Card>
+                ))}
+            </View>
+        );
+    };
+
+    if (loading) {
+        return (
+            <View style={styles.loading}>
+                <ActivityIndicator animating={true} color={Colors.blue800} />
+            </View>
+        );
+    }
+
     return (
-        <View>
-            <Text>All Orders Page</Text>
+        <View style={styles.container}>
+            <Text
+                style={{ fontSize: 20, fontWeight: "bold", marginVertical: 10 }}
+            >
+                Manage All Orders
+            </Text>
+            <FlatList
+                data={allOrders}
+                keyExtractor={(item) => item._id}
+                renderItem={renderItem}
+                numColumns={1}
+            />
         </View>
     );
 };
 
 export default AllOrders;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    loading: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    box: {
+        borderWidth: 1,
+        borderColor: "lightgray",
+        borderRadius: 10,
+        margin: 10,
+        padding: 8,
+        width: 330,
+    },
+    card: {
+        margin: 5,
+        borderRadius: 5,
+    },
+});
