e3c1730f6d4189ea665b0fc49b011f274f7baac7
Pranta07 <palpranta@gmail.com>
2021-12-25 01:14:24 +0600

    all orders page added

--- components/AdminRoute/AdminRoute.js
+++ components/AdminRoute/AdminRoute.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import { ActivityIndicator, Colors } from "react-native-paper";
 import { Navigate } from "react-router";
--- components/AllOrders/AllOrders.js
+++ components/AllOrders/AllOrders.js
@@ -1,6 +1,21 @@
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const AllOrders = () => {
+    const [allOrders, setAllOrders] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(`https://mighty-thicket-60343.herokuapp.com/manage/orders`)
+            .then((res) => res.json())
+            .then((data) => {
+                // console.log(data);
+                setAllOrders(data);
+                setLoading(false);
+            });
+    }, []);
+
     return (
         <View>
             <Text>All Orders Page</Text>
